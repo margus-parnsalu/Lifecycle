62d5029dca74338df8469ea9b1fdafd3b36b604f
margus-parnsalu <margus@MBook.local>
2015-08-24 22:44:19 +0300

    Logging

--- arhea/views.py
+++ arhea/views.py
@@ -4,13 +4,22 @@ General application views
 from pyramid.view import view_config
 from pyramid.security import authenticated_userid
 
+import logging
+log = logging.getLogger(__name__)
+
+
 
 @view_config(route_name='home', renderer='home.jinja2', request_method='GET', permission='view')
 def home(request):
     """Homepage view"""
     project_name = 'Applications'
+
+    #Logging example
+    log.info('Returning project name: %s', project_name)
+
     #Python debugger for troubleshooting
     #import pdb; pdb.set_trace()
+
     return {'project': project_name,
             'logged_in': authenticated_userid(request)}
 
