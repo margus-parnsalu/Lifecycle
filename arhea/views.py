2174f6ab3fd13de071eb0ea873e831ca1ebcc8c1
Margus Pärnsalu <Margus.Parnsalu@Elion.ee>
2015-10-07 13:36:40 +0300

    git deploy test

--- arhea/views.py
+++ arhea/views.py
@@ -9,19 +9,6 @@ log = logging.getLogger(__name__)
 
 
 
-@view_config(route_name='home', renderer='home.jinja2', request_method='GET', permission='view')
-def home(request):
-    """Homepage view"""
-    project_name = 'Telekom rakenduste ja vastutajate nimekiri.'
-
-    #Logging example
-    #log.info('Returning project name: %s', project_name)
-
-    #Python debugger for troubleshooting
-    #import pdb; pdb.set_trace()
-
-    return {'project': project_name,
-            'logged_in': request.authenticated_userid}
 
 
 
