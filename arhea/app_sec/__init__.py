2cb916b2bdd0af79a2c23588e3a5fd0d3c43dea6
Margus Pärnsalu <Margus.Parnsalu@Elion.ee>
2015-10-27 20:05:31 +0200

    app_sec pep8

--- arhea/app_sec/__init__.py
+++ arhea/app_sec/__init__.py
@@ -6,9 +6,10 @@ Security package providing:
 __author__ = 'margusp'
 """
 from ldap3 import Server, Connection, ALL
-#LDAP config
+# LDAP config
 server = Server('ldap.elion.ee', use_ssl=True, get_info=ALL)
 conn = Connection(server=server, auto_bind=True)
+
 
 def include(config):
 
@@ -19,23 +20,21 @@ def include(config):
     conn.authentication = 'SIMPLE'
     conn.bind()
 
-
-    #Sec module template location
+    # Sec module template location
     config.add_jinja2_search_path('arhea:app_sec/templates')
 
-    #Security login/logout
+    # Security login/logout
     config.add_route('login', '/login')
     config.add_route('logout', '/logout')
 
-    #Security_user
+    # Security_user
     config.add_route('user_view', '/sec/users')
     config.add_route('user_view:page', '/sec/users/page/{page:\d+}')
     config.add_route('user_add', '/sec/users/add')
     config.add_route('user_edit', '/sec/users/{usr_id:\d+}/edit')
 
-    #Security_group
+    # Security_group
     config.add_route('group_view', '/sec/groups')
     config.add_route('group_view:page', '/sec/groups/page/{page:\d+}')
     config.add_route('group_add', '/sec/groups/add')
     config.add_route('group_edit', '/sec/groups/{gro_id:\d+}/edit')
-
