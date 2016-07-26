e4e73a0dd8c7af8587d260b538a438b983a9de9e
Margus Pärnsalu <Margus.Parnsalu@Elion.ee>
2016-07-26 13:41:40 +0300

    Celery beat

---
+++ celeryconfig.py
@@ -0,0 +1,21 @@
+from celery import Celery
+from celery.signals import worker_init
+
+from sqlalchemy import engine_from_config
+from arhea.models import (DBSession, Base)
+
+
+# When worker starts this gets the config from .ini file
+@worker_init.connect
+def bootstrap_pyramid(signal, sender):
+    import os
+    from pyramid.paster import bootstrap
+    sender.app.settings = bootstrap('../dev.ini')['registry'].settings
+
+    #SqlAlchemy:
+    engine = engine_from_config(sender.app.settings, 'sqlalchemy.default.')
+    DBSession.configure(bind=engine)
+    Base.metadata.bind = engine
+
+app = Celery('tasks', broker='redis://localhost:6379/0')
+
---
+++ celerytasks/tasks.py
@@ -0,0 +1,30 @@
+import transaction
+from datetime import timedelta
+
+from celeryconfig import app
+
+from arhea.app_sd.actions import CIAction
+
+
+
+
+# Celery beat config
+app.conf.update(
+    CELERYBEAT_SCHEDULE = {
+        'replicate_ci': {
+            'task': 'tasks.replicate_ci',
+            'schedule': timedelta(minutes=30)
+        },
+    }
+)
+
+
+@app.task
+def replicate_ci():
+
+    host = app.settings['sd.host']
+    user = app.settings['sd.user']
+    pwd = app.settings['sd.pwd']
+
+    CIAction.replicate_ci(user, pwd, host)
+    transaction.commit()
